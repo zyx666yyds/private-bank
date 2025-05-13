fd5e92b69e8d6b4d1044454c0a6d8bef2f385e60
Sean <2408893294@qq.com>
2025-05-13 09:19:06 +0800

    增加common模块，自定义封装返回类及错误码等

---
+++ bank-common/src/test/java/com/chinasoft/bankcommon/BankCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinasoft.bankcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
