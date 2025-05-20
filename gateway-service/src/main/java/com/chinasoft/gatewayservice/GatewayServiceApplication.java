0c76465c971b6d0f3fe22e7f52984d227899555d
Sean <2408893294@qq.com>
2025-05-20 14:02:37 +0800

    添加注册中心配置

--- gateway-service/src/main/java/com/chinasoft/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/chinasoft/gatewayservice/GatewayServiceApplication.java
@@ -3,8 +3,10 @@ package com.chinasoft.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
