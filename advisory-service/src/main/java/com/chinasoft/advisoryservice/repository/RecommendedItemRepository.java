08f61e22597f728917c5159a8e7e2f8ca3142735
Jairus <13892164+anotirri@user.noreply.gitee.com>
2025-05-22 17:52:59 +0800

    推荐、交易模块大致完成

--- advisory-service/src/main/java/com/chinasoft/advisoryservice/repository/RecommendedItemRepository.java
+++ advisory-service/src/main/java/com/chinasoft/advisoryservice/repository/RecommendedItemRepository.java
@@ -3,5 +3,9 @@ package com.chinasoft.advisoryservice.repository;
 import com.chinasoft.advisoryservice.model.entity.RecommendationItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecommendedItemRepository extends JpaRepository<RecommendationItem,String> {
+
+    List<RecommendationItem> findByRecommendationId(String recommendationId);
 }
--- advisory-service/src/main/java/com/chinasoft/advisoryservice/service/impl/RecommendationServiceImpl.java
+++ advisory-service/src/main/java/com/chinasoft/advisoryservice/service/impl/RecommendationServiceImpl.java
@@ -1,14 +1,19 @@
 package com.chinasoft.advisoryservice.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.chinasoft.advisoryservice.enums.TradeType;
 import com.chinasoft.advisoryservice.model.dto.RecommendationDTO;
+import com.chinasoft.advisoryservice.model.dto.TradeDTO;
 import com.chinasoft.advisoryservice.model.entity.Recommendation;
 import com.chinasoft.advisoryservice.model.entity.RecommendationItem;
 import com.chinasoft.advisoryservice.repository.RecommendationRepository;
 import com.chinasoft.advisoryservice.repository.RecommendedItemRepository;
 import com.chinasoft.advisoryservice.service.RecommendationService;
+import com.chinasoft.bankcommon.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +27,8 @@ public class RecommendationServiceImpl implements RecommendationService {
     private final RecommendationRepository recommendationRepository;
 
     private final RecommendedItemRepository recommendedItemRepository;
+
+    private final RestTemplate restTemplate;
 
     @Override
     public void createRecommendation(RecommendationDTO recommendationDTO) {
@@ -42,9 +49,29 @@ public class RecommendationServiceImpl implements RecommendationService {
 
     @Override
     public void adoptRecommendation(String recommendationId) {
+        // 客户采纳推荐
         Optional<Recommendation> optional = recommendationRepository.findById(recommendationId);
         Recommendation recommendation = optional.orElseThrow(() -> new RuntimeException("推荐不存在"));
         recommendation.setAccepted(true);
+        recommendationRepository.save(recommendation);
+
+        //生成交易
+        List<RecommendationItem> items = recommendedItemRepository.findByRecommendationId(recommendationId);
+        for (RecommendationItem item : items) {
+            TradeDTO tradeDTO = new TradeDTO();
+            tradeDTO.setClientId(recommendation.getClientId());
+            tradeDTO.setAmount(item.getAmount());
+            tradeDTO.setProductCode(item.getProductCode());
+            tradeDTO.setType(TradeType.SUBSCRIBE);
+            System.out.println(JSONUtil.parse(tradeDTO));
+            restTemplate.postForObject("http://localhost:9011/trades", JSONUtil.parse(tradeDTO), BaseResponse.class);
+        }
+    }
+
+    @Override
+    public void updateRecommendationFeedback(String recommendationId, String feedback) {
+        Recommendation recommendation = recommendationRepository.findById(recommendationId).orElseThrow(() -> new RuntimeException("推荐不存在"));
+        recommendation.setFeedback(feedback);
         recommendationRepository.save(recommendation);
     }
 
